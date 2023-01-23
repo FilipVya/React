3b91ac53bc7469d02b1f8922b9a183b67c8c1ee1
Vyacheslav Filippov <vyacheslav.filippov@iridato.com>
2023-01-23 20:34:17 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return <header className={classes.header}>
+        <img src='https://img.freepik.com/free-vector/colorful-bird-illustration-gradient_343694-1741.jpg?w=740&t=st=1674064999~exp=1674065599~hmac=ee46ce55c29b1e3c390bc9917cba6ed44e1bee4e50dbcb85fd461c8ada612b2b'></img>
+    </header>
+}
+
+export default Header;
