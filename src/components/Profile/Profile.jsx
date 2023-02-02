1a9e29be095f25dc153bdd5c2f768905e058e90f
Vyacheslav Filippov <vyacheslav.filippov@iridato.com>
2023-02-02 19:25:37 +0300

    Dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts';
 import classes from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div>
         <div>
             <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
         </div>
